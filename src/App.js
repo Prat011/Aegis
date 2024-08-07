158d64dab34ba3529c5f8d00a3a1efde8c0791ad
Prathit-tech <ppjoshi2100@gmail.com>
2024-08-07 11:53:20 +0530

    website rough structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AegisWebsite from './components/AegisWebsite';
+
+function App() {
+  return (
+    <div className="App">
+      <AegisWebsite />
+    </div>
+  );
+}
+
+export default App;
